02f2731b110dab49e1c13cc991b19f5c44af9b05
Abraham  Gonzalez <ag32205@srcs.loc>
2018-03-23 11:51:24 -0700

    intro to add item function

--- Shopping_List.js
+++ Shopping_List.js
@@ -3,5 +3,10 @@ button1.addEventListener("click", AddItem);
 
 
 function AddItem() {
+     var ul = document.getElementsByTagName('ul')[0];
+var Newitem = document.getElementById('item');
+    var create = document.createElement('li');
+    create.textContent = Newitem;
+    ul.appendChild(create);
 
 }
