22c8067452e1e1afc1b7a2e39e8996301e0d4981
Abraham  Gonzalez <ag32205@srcs.loc>
2018-03-23 12:03:35 -0700

    ad button and function

--- Shopping_List.js
+++ Shopping_List.js
@@ -4,7 +4,7 @@ button1.addEventListener("click", AddItem);
 
 function AddItem() {
      var ul = document.getElementsByTagName('ul')[0];
-var Newitem = document.getElementById('item');
+var Newitem = document.getElementById('item').value;
     var create = document.createElement('li');
     create.textContent = Newitem;
     ul.appendChild(create);
