9b3542a9286e64f8fd7a3a258e6b9318164691e9
Maxwell L Rahe <mr03989@srcs.loc>
2018-03-27 11:55:47 -0700

    fdsjmnk

--- Shopping_List.js
+++ Shopping_List.js
@@ -1,13 +1,26 @@
 var button1 = document.getElementById("add");
 button1.addEventListener("click", AddItem);
 
-
 function AddItem() {
     var ul = document.getElementsByTagName('ul')[0];
     var Newitem = document.getElementById('item').value;
     var create = document.createElement('li');
     create.textContent = Newitem;
     ul.appendChild(create);
-    var deleteButton = document.getElementById
+    var button = document.createElement("button");
+    button.textContent = "Delete";
+    ul.appendChild(button);
+}
+
+var button2 = document.getElementById("delete");
+button2.addEventListener("click", DeleteItem);
+
+function DeleteItem() {
+    var ul = document.getElementsByTagName('ul')[0];
+    var Newitem = document.getElementById('item').value;
+    var create = document.createElement('li');
+    create.textContent = Newitem;
+    ul.appendChild(create);
+    Newitem.remove();
 }
 
